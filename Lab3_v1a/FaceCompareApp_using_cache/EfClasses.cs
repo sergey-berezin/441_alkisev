0290be6c4d7007e5fa4d5dae7e8b1ab2178073ad
i-alkisev-win10 <i.alkisev@gmail.com>
2022-11-07 04:42:55 +0300

    Add Lab3

---
+++ Lab3_v1a/FaceCompareApp_using_cache/EfClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Navigation;
+using System.Security.Cryptography;
+
+namespace EfClasses
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public byte[] Embedding { get; set; }
+        public ImageDetails Details { get; set; }
+
+        public static string GetHash(byte[] data)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return string.Concat(sha256.ComputeHash(data).Select(x => x.ToString("X2")));
+            }
+        }
+    }
+
+    public class ImageDetails   
+    {
+        [Key]
+        [ForeignKey(nameof(Image))]
+        public int Id { get; set; }
+        public byte[] Data { get; set; }
+    }
+
+    public class ImagesContext: DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImageDetails> Details { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Data Source=images.db");
+        }
+    }
+}
---
+++ Lab3_v1a/FaceCompareApp_using_cache/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FaceCompareApp
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+
+        public void Execute(object parameter) => execute?.Invoke(parameter);
+    }
+}
